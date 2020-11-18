085fb34737151f21ee2d4536405c36a2feafafbd
ahmed <contact.ahmedbm@gmail.com>
2020-11-18 23:39:06 +0100

    single product up

--- front-app/src/components/SingleProductPage/singleProduct.svelte
+++ front-app/src/components/SingleProductPage/singleProduct.svelte
@@ -1,3 +1,7 @@
+<script>
+  let colors = ["0e80f6", "d40019", "ffffff", "737372"];
+</script>
+
 <style>
   .container {
     background-color: #1e1e20;
@@ -42,12 +46,13 @@
   .p_container {
     display: grid;
     grid-template-columns: 1fr[col_1] 1fr [col_2 ]1fr [col_3];
+    padding: 0 30px 0 30px;
   }
 
   .p_info {
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    margin: 150px 0 0 0;
   }
 
   .p_imgs {
@@ -80,7 +85,7 @@
   .p_order_details {
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    margin: 150px 0 0 0;
   }
   .circle_border {
     position: absolute;
@@ -98,6 +103,111 @@
     margin-left: 50%;
     top: 50%;
     transform: translate3d(-50%, -50%, 0);
+  }
+
+  .p_title {
+    color: white;
+    font-size: 35px;
+    font-weight: 700;
+  }
+  .p_price {
+    color: white;
+    font-size: 35px;
+    font-weight: 700;
+  }
+  .p_price span {
+    color: #0e80f6;
+  }
+  .p_subTitle {
+    color: #505c61;
+    font-size: 35px;
+    font-weight: 700;
+  }
+  .p_subTitle span {
+    color: #0e80f6;
+    margin-left: 8px;
+  }
+  .p_info hr {
+    width: 40%;
+    color: #505c61;
+    border: solid 1px #505c61;
+    margin: 5px 0 5px 0;
+  }
+  .p_desc {
+    font-size: 20px;
+    font-weight: 200;
+    color: white;
+  }
+  .p_order_details .title {
+    margin-right: 10px;
+    font-size: 18px;
+    font-weight: 600;
+  }
+  .starts i{
+    color:#4f595a;
+  }
+  .starts .active {
+    color: #ffc107;
+  }
+  .rate {
+    font-size: 18px;
+    font-weight: 600;
+    margin-right: 5px;
+  }
+
+  .p_size {
+    display: flex;
+    flex-direction: row;
+  }
+  .sizes {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+  .size {
+    background-color: #a4a6a9;
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    color: white;
+    margin: 0 5px 0 5px;
+     display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+  .size:hover {
+    background-color: #0e80f6;
+  }
+  .p_colors {
+    display: flex;
+    flex-direction: row;
+  }
+  .colors {
+    display: flex;
+    flex-direction: row;
+  }
+  .color {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+  }
+
+  .color_border {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    cursor: pointer;
+  }
+  .color_border:hover {
+    border-width: 2px !important;
+  }
+  .p_order_details > div{
+    margin: 15px 0 15px 35px;
   }
 </style>
 
@@ -125,12 +235,17 @@
   <div class="p_container">
 
     <div class="p_info">
-      <span class="p_title">
-        T-Shirt
-        <span class="p_price">50 TND</span>
+      <span class="p_title">T-Shirt</span>
+      <span class="p_subTitle">
+        Best T-shirts
+        <span>quality</span>
       </span>
-      <span class="p_subTitle">Best T-shirts quality</span>
       <hr />
+      <span class="p_price">
+        50
+        <span>TND</span>
+      </span>
+
       <div class="p_desc">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
         maxime provident rerum dolorum, quia accusantium cupiditate natus
@@ -163,10 +278,11 @@
       <div class="p_review">
         <span class="title">Review:</span>
         <span class="starts">
-          <i class="fas fa-star" />
-          <i class="fas fa-star" />
-          <i class="fas fa-star" />
-          <i class="fas fa-star" />
+
+          <i class="fas fa-star active" />
+          <i class="fas fa-star active" />
+          <i class="fas fa-star active" />
+          <i class="fas fa-star active" />
           <i class="fas fa-star" />
         </span>
         <span class="rate">4 (75)</span>
@@ -175,23 +291,23 @@
       <div class="p_colors">
         <span class="title">Colors:</span>
         <span class="colors">
-          <span class="color" />
-          <span class="color" />
-          <span class="color" />
-          <span class="color" />
-          <span class="color" />
+          {#each colors as color}
+            <div class="color_border" style="border:0px solid #{color};">
+              <div class="color" style="background-color:#{color}" />
+            </div>
+          {/each}
         </span>
       </div>
 
       <div class="p_size">
         <span class="title">Size:</span>
         <span class="sizes">
-          <span class="size">XS</span>
-          <span class="size">S</span>
-          <span class="size">M</span>
-          <span class="size">L</span>
-          <span class="size">XL</span>
-          <span class="size">XXL</span>
+          <div class="size">XS</div>
+          <div class="size">S</div>
+          <div class="size">M</div>
+          <div class="size">L</div>
+          <div class="size">XL</div>
+          <div class="size">XXL</div>
         </span>
       </div>
 
