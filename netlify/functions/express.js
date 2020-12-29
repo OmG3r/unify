0bb3aa56735667336702c84f8166de4955dca0f6
OmG3r <adam.boulila@live.fr>
2020-12-29 23:13:10 +0100

    up

--- netlify/functions/express.js
+++ netlify/functions/express.js
@@ -1,6 +1,22 @@
-exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    };
-}
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.end('hello')
+});
+router.get('/another', (req, res) => {
+    res.json({ route: req.originalUrl })
+});
+router.post('/', (req, res) => {
+    res.json({ postBody: req.body })
+});
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/express', router);  // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);
