5773856cb83947c59c7f3a4568adffd75d00840b
OmG3r <adam.boulila@live.fr>
2020-12-29 22:53:25 +0100

    up

--- netlify/functions/express.js
+++ netlify/functions/express.js
@@ -1,21 +1,6 @@
-'use strict';
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-
-
-
-
-app.get('/', (res, req) => {
-  
-    req.end("base");
-});
-app.get('/one', (res, req) => {
-  
-  req.end("yes");
-});
-app.post('/updatestate', (req, res) => {
-    res.end('end')
-  });
-
-module.exports.handler = serverless(app);
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Hello World"})
+    };
+}
