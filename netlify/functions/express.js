8022eaafd35448025e76a8a5df14730436c04876
OmG3r <adam.boulila@live.fr>
2020-12-29 23:20:58 +0100

    up

--- netlify/functions/express.js
+++ netlify/functions/express.js
@@ -4,6 +4,17 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://unify-tn.firebaseio.com"
+});
+
+
+
 const router = express.Router();
 router.get('/', (req, res) => {
     res.end('hello')
@@ -14,6 +25,19 @@ router.get('/another', (req, res) => {
 router.post('/', (req, res) => {
     res.json({ postBody: req.body })
 });
+
+app.post('/initializeClaims', async (req, res) => {
+
+    let resp = await admin.auth().setCustomUserClaims(req.body.uid, req.body.claims)
+    console.log(resp)
+    res.end(JSON.stringify(resp))
+})
+
+app.post('/getClaims', async (req, res) => {
+    let userRecord = await admin.auth().getUser(req.body.uid)
+    console.log(userRecord)
+    res.end(JSON.stringify(userRecord, null, 4))
+})
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/express', router);  // path must route to lambda
