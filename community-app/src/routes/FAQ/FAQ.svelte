c7d4df68f8a5fdf848aa0d9be6cab44d6848d5f1
omg3r <adam.boulila@live.fr>
2021-03-26 20:29:55 +0100

    up

---
+++ community-app/src/routes/FAQ/FAQ.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {Route} from 'tinro'; 
+    import Articlator from './Articlator.svelte'
+</script>
+
+
+<Route path="/">
+    main faq
+
+</Route>
+
+<Route path="/:article">
+    <Articlator />
+
+</Route>
---
+++ community-app/src/routes/FAQ/SideNav.svelte
@@ -0,0 +1,28 @@
+<style>
+    .u-side-nav {
+        width: 300px;
+        display: flex;
+        flex-direction: column;
+        padding: 10px 10px 10px 20px;
+
+        max-height: 100vh;
+        min-height: 100vh;
+        height: 100%;
+        position: sticky;
+        top: 0;
+
+        overflow-y: scroll;
+    }
+    
+</style>
+
+<script>
+    import {faqs} from '../../store'
+    import AccordionItem from './AccordionItem.svelte'
+</script>
+
+<div class="u-side-nav">
+    {#each Object.entries(faqs) as [dropdownTitle, links]}
+        <AccordionItem {dropdownTitle} {links}  />
+    {/each}
+</div>
